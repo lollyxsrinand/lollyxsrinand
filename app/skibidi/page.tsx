6c9c6340b03d2fd3fdfcc2f681104d771c47427a
Srinand Diddi <srinand.com@gmail.com>
2025-10-14 22:41:18 +0530

    added skibidi

---
+++ app/skibidi/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Skibidi = () => {
+  return (
+    <div className='h-screen w-full flex flex-col justify-center items-center gap-4'>
+      <a className="text-blue-400 font-bold text-2xl" href="https://colab.research.google.com/drive/1jR906H44i2YttwzJr9dwW9HNeJhyDEy5?usp=sharing">Week 6</a>
+      <a className="text-blue-400 font-bold text-2xl" href="https://colab.research.google.com/drive/1vMozden_1ktv5mmkfAeWBQ6eQWHIQmIN?usp=sharing">Week 7</a>
+      <a className="text-blue-400 font-bold text-2xl" href="https://colab.research.google.com/drive/1NrCJ1qV2JX5AzN5X_Sa_VdY2dk-maEf8?usp=sharing">Week 8</a>
+      <a className="text-blue-400 font-bold text-2xl" href="https://colab.research.google.com/drive/1wUtNfkA8WWjDArhxZf9VReas6kq939be?usp=sharing">Week 9</a>
+      <a className="text-blue-400 font-bold text-2xl" href="https://colab.research.google.com/drive/1-HIz4vAcLiMC_uNiHsQb9jJxc0T9NUub?usp=sharing">Week 10</a>
+    </div>
+  )
+}
+
+export default Skibidi 
